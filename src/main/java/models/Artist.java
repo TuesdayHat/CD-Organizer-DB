9496cbe8e7754a79496e28cf53a22858f61ba29d
Adam Calhoun <a.t.calhoun@gmail.com>
2018-01-28 13:56:00 -0800

    add Equals and hashCode overrides to artist pojo

--- src/main/java/models/Artist.java
+++ src/main/java/models/Artist.java
@@ -8,5 +8,27 @@ public class Artist {
     this.name = name;
   }
 
+  public String getName(){return this.name;}
+  public void setName(String nName){this.name = nName;}
 
+  public int getId(){return this.id;}
+  public void setId(int nId){this.id = nId;}
+
+  @Override
+  public boolean equals(Object o){
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Artist artist = (Artist) o;
+
+    if (id != artist.id) return false;
+    return name.equals(artist.name);
+  }
+
+  @Override
+  public int hashCode(){
+    int result = this.name.hashCode();
+    result = 31*result+id;
+    return result;
+  }
 }
