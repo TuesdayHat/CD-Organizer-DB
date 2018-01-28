54bc5a46d2b0eb38ff6e863a5125ee1ff986b78a
Adam Calhoun <a.t.calhoun@gmail.com>
2018-01-28 13:37:44 -0800

    add Equals and hashCode overrides to CD pojo

--- src/main/java/models/Artist.java
+++ src/main/java/models/Artist.java
@@ -1,4 +1,12 @@
 package models;
 
 public class Artist {
+  private String name;
+  private int id;
+
+  public Artist(String name) {
+    this.name = name;
+  }
+
+
 }
--- src/main/java/models/CD.java
+++ src/main/java/models/CD.java
@@ -3,29 +3,53 @@ package models;
 import java.util.ArrayList;
 
 public class CD {
-    private String title;
-    private String artist;
-    private static ArrayList<CD> instances = new ArrayList<>();
-    private int id;
+  private String title;
+  private String artist;
+  private static ArrayList<CD> instances = new ArrayList<>();
+  private int id;
+  private int artistId;
 
-    public CD(String title, String artist){
-        this.title = title;
-        this.artist = artist;
-        instances.add(this);
-    }
+  public CD(String title, String artist){
+    this.title = title;
+    this.artist = artist;
+    instances.add(this);
+  }
 
-    public String getTitle() {
-        return this.title;
-    }
-    public static ArrayList<CD> getInstances(){
-        return instances;
-    }
+  public String getTitle() {
+      return this.title;
+  }
+  public static ArrayList<CD> getInstances(){
+      return instances;
+  }
 
-    public String getArtist() {
-        return this.artist;
-    }
+  public String getArtist() {
+      return this.artist;
+  }
 
-    public int getId(){ return this.id; }
-    public void setId(int nId) {this.id = nId;}
+  public int getId(){ return this.id; }
+  public void setId(int nId) {this.id = nId;}
+  public int getArtistId(){return this.artistId;}
+  public void setArtistId(int id) {this.artistId = id;}
+
+  @Override
+  public boolean equals(Object o){
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    CD cd = (CD) o;
+
+    if (id != cd.id) return false;
+    if (artistId != cd.artistId) return false;
+    return title.equals(cd.title);
+  }
+
+  @Override
+  public int hashCode(){
+    int result = this.title.hashCode();
+    result = 31*result+id;
+    result = 31*result+artistId;
+
+    return result;
+  }
 
 }
